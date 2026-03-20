25546435f3228d55bd18983dfa9eef3941b08410
Lucas Reichow <lucasreichow@Mac.fritz.box>
2026-03-20 20:09:11 +0100

    feat: add right-aligned Zurueck button in student rows

--- script.js
+++ script.js
@@ -1619,7 +1619,10 @@ function renderStudentsModule() {
         
         row.innerHTML = `
             <td>${index + 1}</td>
-            <td class="${student.learningSupport ? 'learning-support' : ''}">${student.name}</td>
+            <td class="student-name-cell ${student.learningSupport ? 'learning-support' : ''}">
+                <span class="student-name-text">${student.name}</span>
+                <button class="btn btn-sm btn-light" onclick="scrollToTopOfStudentList()">Zurück</button>
+            </td>
             <td style="text-align: center;">
                 <input type="checkbox" ${student.learningSupport ? 'checked' : ''} onclick="toggleLearningSupport(${index}, this)">
             </td>
@@ -1647,6 +1650,13 @@ function renderStudentsModule() {
     // Inhalt auf einmal austauschen
     studentsTable.innerHTML = '';
     studentsTable.appendChild(fragment);
+}
+
+function scrollToTopOfStudentList() {
+    const container = document.querySelector('#schueler-module .student-list-container');
+    if (container) {
+        container.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 }
 
 // Schüler hinzufügen
